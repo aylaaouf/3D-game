1502f89225f60a131d1a9c2fa30f9360198fbbcb
aelasefa <aymanelasefar1337@gmail.com>
2025-07-31 11:18:58 +0100

    Change game colors, add player rotation and wall collision for bonus part

--- move_player.c
+++ move_player.c
@@ -14,57 +14,80 @@
 
 void redraw_player_only(t_game *game)
 {
-	int	screen_x, screen_y, py, px;
-    screen_x = game->player.x * TILE_SIZE;
-    screen_y = game->player.y * TILE_SIZE;
-	py = 0;
-    while (py < 4)
+	int screen_x = game->player.x * TILE_SIZE;
+	int screen_y = game->player.y * TILE_SIZE;
+	for (int py = 0; py < 4; py++)
 	{
-		px = 0;
-        while (px < 4)
+		for (int px = 0; px < 4; px++)
 		{
-			mlx_pixel_put(game->mlx, game->win, screen_x + px, 
-			   screen_y + py, 0xFF0000);
-			px++;
+			mlx_pixel_put(game->mlx, game->win,
+				screen_x + px, screen_y + py, 0xFF0000);
 		}
-		py++;
 	}
 }
 
-int	handle_keypress(int keycode, t_game *game)
+int handle_keypress(int keycode, t_game *game)
 {
 	double move_speed = 0.1;
-	double new_x = game->player.x;
-	double new_y = game->player.y;
+	double rot_speed = 0.1;
+	double new_x, new_y;
 
-	if (keycode == 65307)
+	if (keycode == 65307) // ESC
 		exit(0);
 
+	// Move Forward
 	if (keycode == 'w' || keycode == 65362)
 	{
-		new_x += game->player.dir_x * move_speed;
-		new_y += game->player.dir_y * move_speed;
+		new_x = game->player.x + game->player.dir_x * move_speed;
+		new_y = game->player.y + game->player.dir_y * move_speed;
+		if (game->map->map[(int)new_y][(int)(game->player.x)] != '1')
+			game->player.y = new_y;
+		if (game->map->map[(int)(game->player.y)][(int)new_x] != '1')
+			game->player.x = new_x;
 	}
+
+	// Move Backward
 	if (keycode == 's' || keycode == 65364)
 	{
-		new_x -= game->player.dir_x * move_speed;
-		new_y -= game->player.dir_y * move_speed;
+		new_x = game->player.x - game->player.dir_x * move_speed;
+		new_y = game->player.y - game->player.dir_y * move_speed;
+		if (game->map->map[(int)new_y][(int)(game->player.x)] != '1')
+			game->player.y = new_y;
+		if (game->map->map[(int)(game->player.y)][(int)new_x] != '1')
+			game->player.x = new_x;
 	}
-	if (keycode == 'a' || keycode == 65361)
-	{
-		new_x -= game->player.plane_x * move_speed;
-		new_y -= game->player.plane_y * move_speed;
-	}
+
+	// Rotate Right
 	if (keycode == 'd' || keycode == 65363)
 	{
-		new_x += game->player.plane_x * move_speed;
-		new_y += game->player.plane_y * move_speed;
+		double old_dir_x = game->player.dir_x;
+		game->player.dir_x = game->player.dir_x * cos(-rot_speed)
+			- game->player.dir_y * sin(-rot_speed);
+		game->player.dir_y = old_dir_x * sin(-rot_speed)
+			+ game->player.dir_y * cos(-rot_speed);
+		double old_plane_x = game->player.plane_x;
+		game->player.plane_x = game->player.plane_x * cos(-rot_speed)
+			- game->player.plane_y * sin(-rot_speed);
+		game->player.plane_y = old_plane_x * sin(-rot_speed)
+			+ game->player.plane_y * cos(-rot_speed);
 	}
-	if (game->map->map[(int)new_y][(int)new_x] != '1')
+
+	// Rotate Left
+	if (keycode == 'a' || keycode == 65361)
 	{
-		game->player.x = new_x;
-		game->player.y = new_y;
+		double old_dir_x = game->player.dir_x;
+		game->player.dir_x = game->player.dir_x * cos(rot_speed)
+			- game->player.dir_y * sin(rot_speed);
+		game->player.dir_y = old_dir_x * sin(rot_speed)
+			+ game->player.dir_y * cos(rot_speed);
+		double old_plane_x = game->player.plane_x;
+		game->player.plane_x = game->player.plane_x * cos(rot_speed)
+			- game->player.plane_y * sin(rot_speed);
+		game->player.plane_y = old_plane_x * sin(rot_speed)
+			+ game->player.plane_y * cos(rot_speed);
 	}
+
+	// Refresh scene
 	raycast(game);
 	render_minimap(game);
 	return (0);
--- raycast.c
+++ raycast.c
@@ -17,50 +17,52 @@ void	draw_vertical_line(t_game *game, int x, int draw_start, int draw_end, int c
 	for (int y = draw_start; y <= draw_end; y++)
 		mlx_pixel_put(game->mlx, game->win, x, y, color);
 }
-
 void	raycast(t_game *game)
 {
-	int x = 0;
-	while (x < SCREEN_WIDTH)
+	for (int x = 0; x < SCREEN_WIDTH; x++)
 	{
 		// 1. Calculate ray position and direction
 		double camera_x = 2 * x / (double)SCREEN_WIDTH - 1;
 		double ray_dir_x = game->player.dir_x + game->player.plane_x * camera_x;
 		double ray_dir_y = game->player.dir_y + game->player.plane_y * camera_x;
 
-		// 2. Which box of the map we're in
+		// 2. Map square the player is in
 		int map_x = (int)game->player.x;
 		int map_y = (int)game->player.y;
 
-		// 3. Length of ray from current position to next x or y side
+		// 3. Length of ray to next x or y side
 		double delta_dist_x = (ray_dir_x == 0) ? 1e30 : fabs(1 / ray_dir_x);
 		double delta_dist_y = (ray_dir_y == 0) ? 1e30 : fabs(1 / ray_dir_y);
 
-		// 4. What direction to step in x or y
-		int step_x;
+		double side_dist_x, side_dist_y;
+		int step_x, step_y;
+
+		// 4. Calculate step and initial sideDist
 		if (ray_dir_x < 0)
+		{
 			step_x = -1;
-		else 
-			step_x = 1;
-		int step_y;
-		if (ray_dir_y < 0)
-			step_y = -1;
-		else 
-			step_y = 1;
-		double side_dist_x;
-		if (ray_dir_x < 0)
 			side_dist_x = (game->player.x - map_x) * delta_dist_x;
+		}
 		else
+		{
+			step_x = 1;
 			side_dist_x = (map_x + 1.0 - game->player.x) * delta_dist_x;
-		double side_dist_y;
+		}
 		if (ray_dir_y < 0)
+		{
+			step_y = -1;
 			side_dist_y = (game->player.y - map_y) * delta_dist_y;
+		}
 		else
+		{
+			step_y = 1;
 			side_dist_y = (map_y + 1.0 - game->player.y) * delta_dist_y;
+		}
+
 		// 5. Perform DDA
 		int hit = 0;
 		int side;
-		while (hit == 0)
+		while (!hit)
 		{
 			if (side_dist_x < side_dist_y)
 			{
@@ -74,44 +76,38 @@ void	raycast(t_game *game)
 				map_y += step_y;
 				side = 1;
 			}
-			if (game->map->map[map_y][map_x] == '1') // wall hit
+			if (game->map->map[map_y][map_x] == '1')
 				hit = 1;
 		}
 
-		// 6. Calculate distance to wall
+		// 6. Calculate distance projected on camera direction
 		double perp_wall_dist = (side == 0)
-			? ((map_x - game->player.x + (1.0 - (double)step_x) / 2.0) / ray_dir_x)
-			: ((map_y - game->player.y + (1.0 - (double)step_y) / 2.0) / ray_dir_y);
+			? (map_x - game->player.x + (1 - step_x) / 2.0) / ray_dir_x
+			: (map_y - game->player.y + (1 - step_y) / 2.0) / ray_dir_y;
 
 		// 7. Calculate height of line to draw
 		int line_height = (int)(SCREEN_HEIGHT / perp_wall_dist);
+
+		// 8. Calculate lowest and highest pixel to fill
 		int draw_start = -line_height / 2 + SCREEN_HEIGHT / 2;
 		if (draw_start < 0)
 			draw_start = 0;
+
 		int draw_end = line_height / 2 + SCREEN_HEIGHT / 2;
 		if (draw_end >= SCREEN_HEIGHT)
 			draw_end = SCREEN_HEIGHT - 1;
 
-		// 8. Choose color
-		int color;
-		if (side == 1)
-			color = 0xAAAAAA;
-		else
-			color = 0xFF0000;
 		// 9. Draw ceiling
 		for (int y = 0; y < draw_start; y++)
-			mlx_pixel_put(game->mlx, game->win, x, y,
-				rgb(game->config->ceil.r, game->config->ceil.g, game->config->ceil.b));
+			mlx_pixel_put(game->mlx, game->win, x, y, 0x87CEEB);
 
-		// 10. Draw wall
-		draw_vertical_line(game, x, draw_start, draw_end, color);
+		// 10. Draw wall (color can depend on side for shading)
+		int color = (side == 1) ? 0x00FF00 : 0x0000FF;
+		for (int y = draw_start; y <= draw_end; y++)
+			mlx_pixel_put(game->mlx, game->win, x, y, color);
 
 		// 11. Draw floor
 		for (int y = draw_end + 1; y < SCREEN_HEIGHT; y++)
-			mlx_pixel_put(game->mlx, game->win, x, y,
-				rgb(game->config->floor.r, game->config->floor.g, game->config->floor.b));
-
-		x++;
+			mlx_pixel_put(game->mlx, game->win, x, y, 0xFFFFFF);
 	}
 }
-
