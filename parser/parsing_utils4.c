cf75de17e1793766e9577c9f226d9c72ffe48417
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-09-22 14:07:51 +0100

    parse door

--- parser/parsing_utils4.c
+++ parser/parsing_utils4.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 17:38:44 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/07/29 22:20:48 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/09/22 14:05:14 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,42 @@ int	is_map_line(char *line)
 	return (line[0] == '1' || line[0] == ' ');
 }
 
+int	valid_door(t_game *game)
+{
+	int	(i), (j);
+
+	i = 0;
+	game->map->height = ft_strlen_2d(game->map->map);
+	while (i < game->map->height)
+	{
+		j = 0;
+		game->map->last_width = ft_strlen(game->map->map[i]);
+		while (j < game->map->last_width)
+		{
+			if (game->map->map[i][j] == 'P')
+			{
+				if (game->map->map[i][j - 1] != '1' || game->map->map[i][j + 1] != '1')
+					return (1);
+				else if (game->map->map[i - 1][j] == '1' && game->map->map[i + 1][j] == '1'
+					&& game->map->map[i][j - 1] == '1' && game->map->map[i][j + 1] == '1')
+					return (1);
+				else if (game->map->map[i - 1][j] == '1' || game->map->map[i + 1][j] == '1')
+					return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	condition_b(t_game *game, int player)
 {
 	if (player_error(player))
 		return (1);
 	if (is_closed(game))
 		return (1);
+	if (valid_door(game))
+		return (write(2, "Invalid door\n", 14), 1);
 	return (0);
 }
